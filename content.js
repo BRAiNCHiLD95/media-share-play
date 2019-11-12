2ee390d825d6ee04770bbcd661aabde0327686f2
Brian Sam Thomas <thebrainchild95@gmail.com>
2019-11-13 01:13:04 +0530

    Added support for Amazon Prime Video
    Added the ability to inject the controller to Amazon Prime Video's video player.

--- content.js
+++ content.js
@@ -8,12 +8,12 @@
  * between background.js & content.js
  */
 const connectToServices = () => {
-    const initContent = chrome.runtime.connect({ name: "uvpc-b" });
-    console.log("connecting from contentjs", initContent);
-    initContent.postMessage({ initiated: true });
-    chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
-        deployer(msg, initContent);
-    });
+	const initContent = chrome.runtime.connect({ name: "uvpc-b" });
+	console.log("connecting from contentjs", initContent);
+	initContent.postMessage({ initiated: true });
+	chrome.runtime.onMessage.addListener((msg, sender, sendResponse) => {
+		deployer(msg, initContent);
+	});
 };
 
 window.addEventListener("load", connectToServices);
@@ -21,102 +21,140 @@ window.addEventListener("load", connectToServices);
 /**
  * Based on the domain passed, deploy the controller element onto DOM
  * @param {string} msg
+ * @param {Port} port
  */
-const deployer = (msg, port) => {
-    switch (msg.domain) {
-        case "netflix":
-            return deployNetflix(port);
-        case "primevideo":
-            console.log("APV Deploy!");
-        // return deployAPV(port);
-        default:
-            console.error("Don't really need this!");
-    }
+const deployer = async (msg, port) => {
+	var videoElement = await fetchVideoElement();
+	if (!videoElement) return false;
+	updateBadgeText(port, videoElement);
+	var injectedFa = await injectStyles();
+	if (!injectedFa.success) return false;
+	switch (msg.domain) {
+		case "netflix":
+			return deployNetflix(port, videoElement);
+		case "primevideo":
+			return deployAPV(port, videoElement);
+		default:
+			console.error("Don't really need this!");
+	}
 };
 
-const deployNetflix = async port => {
-    var videoElement = await fetchVideoElement();
-    if (!videoElement) return false;
-    updateBadgeText(port, videoElement);
-    var injectedFa = await injectStyles();
-    if (!injectedFa.success) return false;
-    var injectedController = await injectControllerToNetflix();
-    var controllers = document.querySelectorAll("#playbackController");
-    if (controllers.length > 1) {
-        controllers.forEach((element, index) => {
-            if (index > 0) element.remove();
-        });
-    }
-    if (!injectedController.success) return false;
-    document
-        .getElementById("uvpc-btn")
-        .addEventListener("click", playBackToggler);
-    document
-        .getElementById("uvpc-value")
-        .addEventListener("input", event =>
-            valueChanged(port, event, videoElement)
-        );
-    document.getElementById("uvpc-value").value = videoElement.playbackRate;
+/**
+ * Deploys the UVP Controller for Amazon Prime Video
+ * @param {Port} port
+ * @param {HTMLVideoElement} videoElement
+ */
+const deployAPV = async (port, videoElement) => {
+	var injectedController = await injectController("primevideo");
+	if (!injectedController.success) return false;
+    videoElement.onloadeddata = attachListeners(port, videoElement);
+};
+
+/**
+ * Attaches Listeners to the controller
+ * @param {Port} port
+ * @param {HTMLVideoElement} videoElement
+ */
+const attachListeners = (port, videoElement) => {
+    removeDuplicateControllers();
+	document
+		.getElementById("uvpc-btn")
+		.addEventListener("click", playBackToggler);
+	document
+		.getElementById("uvpc-value")
+		.addEventListener("input", event =>
+			valueChanged(port, event, videoElement)
+		);
+	document.getElementById("uvpc-value").value = videoElement.playbackRate;
+};
+
+/**
+ * Deploys the UVP Controller for Netflix
+ * @param {Port} port
+ * @param {HTMLVideoElement} videoElement
+ */
+const deployNetflix = async (port, videoElement) => {
+	var injectedController = await injectController("netflix");
+	if (!injectedController.success) return false;
+	videoElement.onloadeddata = attachListeners(port, videoElement);
+};
+
+
+/**
+ * If something goes wrong and multiple controllers are injected,
+ * this function will remove duplicate controllers.
+ */
+const removeDuplicateControllers = () => {
+	var controllers = document.querySelectorAll("#playbackController");
+	if (controllers.length > 1) {
+		controllers.forEach((element, index) => {
+			if (index > 0) element.remove();
+		});
+	}
 };
 
 /**
  * Builds the icon for the cog controller
+ * @param {String} ottName
  */
-const injectControllerToNetflix = () => {
-    // OTT specific
-    let playerDiv = document.querySelector(
-        ".PlayerControlsNeo__button-control-row"
-    );
-    if (!document.contains(playerDiv)) return false;
-    // cog
-    return new Promise((resolve, reject) => {
-        let main = document.createElement("div");
-        main.id = "playbackController";
-        let videoControllerDiv = document.createElement("div");
-        videoControllerDiv.id = "uvpc-div";
-        videoControllerDiv.innerHTML = `<div class="playbackSpeeds">
+const injectController = ottName => {
+	// OTT specific
+	let playerDiv = document.querySelector(playerDivs[ottName]);
+	if (!document.contains(playerDiv)) return false;
+	// cog
+	return new Promise((resolve, reject) => {
+		let main = document.createElement("div");
+		main.id = "playbackController";
+		let videoControllerDiv = document.createElement("div");
+		videoControllerDiv.id = "uvpc-div";
+		videoControllerDiv.innerHTML = `<div class="playbackSpeeds">
                 <h3 class="playback-header">Playback Rate</h3>
                 <div class="range-stuff">
                     <input id="uvpc-value" type="range" name="speed" data-thumbwidth="20" step="0.25" min="0" max="4">
-                    <output name="rangeVal">1</output>
+                    <output name="rangeVal">1x</output>
                 </div>
             </div>`;
-        let videoControllerBtn = document.createElement("button");
-        videoControllerBtn.id = "uvpc-btn";
-        videoControllerBtn.style.background = "none";
-        videoControllerBtn.style.border = "none";
-        videoControllerBtn.style.fontSize = "inherit";
-        videoControllerBtn.style.marginBottom = "8px";
-        videoControllerBtn.innerHTML = '<i class="fa fa-cogs fa-4x"></fa>';
-        main.innerHTML =
-            videoControllerDiv.outerHTML + videoControllerBtn.outerHTML;
-        playerDiv.insertBefore(main, playerDiv.lastChild);
-        if (!playerDiv.contains(main))
-            reject("Failed to inject the controller");
-        resolve({ success: true, element: main });
-    });
+		let videoControllerBtn = document.createElement("button");
+		videoControllerBtn.id = "uvpc-btn";
+		videoControllerBtn.style.background = "none";
+		videoControllerBtn.innerHTML = '<i class="fa fa-cogs"></fa>';
+		main.innerHTML =
+			videoControllerDiv.outerHTML + videoControllerBtn.outerHTML;
+		playerDiv.insertBefore(main, playerDiv.lastChild);
+		if (!playerDiv.contains(main)) reject("Failed to inject the controller");
+		resolve({ success: true, element: main });
+	});
 };
 
+/**
+ * List of playerDivs from different supported OTTs.
+ */
 const playerDivs = {
-    netflix: ".PlayerControlsNeo__button-control-row",
+	netflix: ".PlayerControlsNeo__button-control-row",
+	primevideo: ".controlsOverlayTopRight .topPanel .right .topButtons .hideableTopButtons",
 };
 
 const playBackToggler = event => {
-    console.log("Event fired!", event);
-    document.getElementById("uvpc-div").classList.toggle("active");
+	document.getElementById("uvpc-div").classList.toggle("active");
 };
 
+/**
+ * When the playbackRate is changed, the output value
+ * and the badgeText is updated to reflect the change.
+ * @param {Port} port
+ * @param {Event} event
+ * @param {HTMLVideoElement} videoElement
+ */
 const valueChanged = (port, event, videoElement) => {
-    console.log(event);
-    var controlMin = Number(event.target.min),
-        controlMax = Number(event.target.max),
-        controlVal = Number(event.target.value),
-        range = controlMax - controlMin,
-        output = event.target.nextElementSibling;
-    position = ((controlVal - controlMin) / range) * 100;
-    output.innerHTML = controlVal + "x";
-    videoElement.playbackRate = event.target.valueAsNumber;
-    updateBadgeText(port, videoElement);
+	var controlMin = Number(event.target.min),
+		controlMax = Number(event.target.max),
+		controlVal = Number(event.target.value),
+		range = controlMax - controlMin,
+		output = event.target.nextElementSibling;
+	position = ((controlVal - controlMin) / range) * 100;
+	output.innerHTML = controlVal + "x";
+	videoElement.playbackRate = event.target.valueAsNumber;
+	updateBadgeText(port, videoElement);
 };
 
 /**
@@ -124,21 +162,21 @@ const valueChanged = (port, event, videoElement) => {
  * and the playback controller to the page
  */
 const injectStyles = () => {
-    return new Promise((resolve, reject) => {
-        let headOfDoc = document.head;
-        let faLink = document.createElement("link");
-        faLink.id = "fa-uvpc";
-        faLink.href =
-            "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
-        faLink.rel = "stylesheet";
-        faLink.integrity =
-            "sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN";
-        faLink.crossOrigin = "anonymous";
-        headOfDoc.appendChild(faLink);
-        if (!headOfDoc.contains(faLink))
-            reject("Failed to inject styles to the Page");
-        resolve({ success: true, element: faLink });
-    });
+	return new Promise((resolve, reject) => {
+		let headOfDoc = document.head;
+		let faLink = document.createElement("link");
+		faLink.id = "fa-uvpc";
+		faLink.href =
+			"https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css";
+		faLink.rel = "stylesheet";
+		faLink.integrity =
+			"sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN";
+		faLink.crossOrigin = "anonymous";
+		headOfDoc.appendChild(faLink);
+		if (!headOfDoc.contains(faLink))
+			reject("Failed to inject styles to the Page");
+		resolve({ success: true, element: faLink });
+	});
 };
 
 /**
@@ -146,17 +184,17 @@ const injectStyles = () => {
  * @returns {HTMLVideoElement}
  */
 const fetchVideoElement = () => {
-    return new Promise((resolve, reject) => {
-        new MutationObserver((mutationRecords, observer) => {
-            Array.from(document.querySelectorAll("video")).forEach(element => {
-                resolve(element);
-                observer.disconnect();
-            });
-        }).observe(document.documentElement, {
-            childList: true,
-            subtree: true,
-        });
-    });
+	return new Promise((resolve, reject) => {
+		new MutationObserver((mutationRecords, observer) => {
+			Array.from(document.querySelectorAll("video")).forEach(element => {
+                if (element.currentSrc !== "") resolve(element);
+				observer.disconnect();
+			});
+		}).observe(document.documentElement, {
+			childList: true,
+			subtree: true,
+		});
+	});
 };
 
 /**
@@ -165,8 +203,8 @@ const fetchVideoElement = () => {
  * @param {HTMLVideoElement} video
  */
 const updateBadgeText = (port, video) => {
-    port.postMessage({
-        badgeText: video.playbackRate,
-        video,
-    });
+	port.postMessage({
+		badgeText: video.playbackRate,
+		video,
+	});
 };
