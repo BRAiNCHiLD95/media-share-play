7226eeec22ad170c6d5e77ed714922e16593bfed
Brian Sam Thomas <thebrainchild95@gmail.com>
2019-11-13 01:14:35 +0530

    Improved JS Docs, and Prettier.

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 /**
- * Background Services
- * These have access to all Chrome APIs & operate in an isolated environment
+ * Background Service
+ * This has access to all Chrome APIs & operates in an isolated environment
  */
 
 /**
@@ -8,62 +8,78 @@
  * channel to communicate between scripts
  */
 const getMessenger = contentMessenger => {
-    if ((contentMessenger.name = "uvpc-b")) {
-        const checkForOTTs = (tabId, changeInfo, tab) => {
-            if (changeInfo.url || changeInfo.audible || changeInfo.status === "complete") {
-                let currentUrl = changeInfo.url || tab.url;
-                if (currentUrl && tab.status === "complete") {
-                    if (currentUrl.match(netflixRegex)) {
-                        sendToContentScript(contentMessenger, {
-                            domain: "netflix",
-                            audible: tab.audible,
-                        }, tabId);
-                        return;
-                    }
-                    if (currentUrl.match(apvRegex)) {
-                        sendToContentScript(contentMessenger, {
-                            domain: "primevideo",
-                            audible: tab.audible,
-                        }, tabId);
-                        return;
-                    }
-                }
-            }
-        };
-        contentMessenger.onMessage.addListener(message => {
-            console.log(message);
-            if (message.initiated) {
-                console.log(`connected to ${contentMessenger.sender.tab.title}`);
-                chrome.tabs.onCreated.addListener(checkForOTTs);
-                chrome.tabs.onUpdated.addListener(checkForOTTs);
-            }
-            if (message.badgeText) {
-                chrome.browserAction.setBadgeText({
-                    text: message.badgeText + "x",
-                    tabId: contentMessenger.sender.tab.id,
-                });
-                chrome.browserAction.setBadgeBackgroundColor({
-                    color: "#000000",
-                    tabId: contentMessenger.sender.tab.id,
-                });
-            }
-        });
-        contentMessenger.onDisconnect.addListener(() => {
-            console.info("disconnected from backgroundjs");
-            chrome.tabs.onCreated.removeListener(checkForOTTs);
-            chrome.tabs.onUpdated.removeListener(checkForOTTs);
-            contentMessenger = null;
-        });
-    }
+	if ((contentMessenger.name = "uvpc-b")) {
+		const checkForOTTs = (tabId, changeInfo, tab) => {
+			if (
+				changeInfo &&
+				(changeInfo.url ||
+					changeInfo.audible ||
+					changeInfo.status === "complete")
+			) {
+				let currentUrl = changeInfo.url || tab.url;
+				if (currentUrl && tab.status === "complete") {
+					if (currentUrl.match(netflixRegex)) {
+						return sendToContentScript(
+							contentMessenger,
+							{
+								domain: "netflix",
+								audible: tab.audible,
+							},
+							tabId
+						);
+					}
+					if (currentUrl.match(apvRegex)) {
+						return sendToContentScript(
+							contentMessenger,
+							{
+								domain: "primevideo",
+								audible: tab.audible,
+							},
+							tabId
+						);
+					}
+				}
+			}
+		};
+		contentMessenger.onMessage.addListener(message => {
+			if (message.initiated) {
+				console.info(`connected to ${contentMessenger.sender.tab.title}`);
+				chrome.tabs.onCreated.addListener(checkForOTTs);
+				chrome.tabs.onUpdated.addListener(checkForOTTs);
+			}
+			if (message.badgeText) {
+				chrome.browserAction.setBadgeText({
+					text: message.badgeText + "x",
+					tabId: contentMessenger.sender.tab.id,
+				});
+				chrome.browserAction.setBadgeBackgroundColor({
+					color: "#000000",
+					tabId: contentMessenger.sender.tab.id,
+				});
+			}
+		});
+		contentMessenger.onDisconnect.addListener(() => {
+			console.info("disconnected from backgroundjs");
+			chrome.tabs.onCreated.removeListener(checkForOTTs);
+			chrome.tabs.onUpdated.removeListener(checkForOTTs);
+			contentMessenger = null;
+		});
+	}
 };
 
+/**
+ * Sends a message to the content-script for specified tab
+ * @param {Port} port
+ * @param {Object} message
+ * @param {Number} tabId
+ */
 const sendToContentScript = (port, message, tabId) => {
-    if (port) {
-        console.log(`SENDING ${JSON.stringify(message, null, 2)} to Tab #${tabId}`);
-        chrome.tabs.sendMessage(tabId, message);
-    } else {
-        console.log("PORT GONE!");
-    }
+	if (port) {
+		console.log(`SENDING ${JSON.stringify(message, null, 2)} to Tab #${tabId}`);
+		chrome.tabs.sendMessage(tabId, message);
+	} else {
+		console.log("PORT GONE!");
+	}
 };
 
 const netflixRegex = /(http(s)?:\/\/.)?(www\.)?(netflix.com\/watch)/;
